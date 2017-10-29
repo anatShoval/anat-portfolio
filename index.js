1b393cce3340087d77c0ca7529f8da71216cc83b
Anat Knebel Shoval <anat.shoval@gmail.com>
2017-10-29 14:03:35 +0200

    fixing port

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.get('/ContactMe.html', function (req, res) {
     })
 })
 
-app.listen(3000, function () {
- console.log('Example app listening on port 3000!')
+
+app.listen(process.env.PORT || 3000, function () {
+ console.log('Anat Portfolio app listening on port ' + (process.env.PORT || 3000))
 })
